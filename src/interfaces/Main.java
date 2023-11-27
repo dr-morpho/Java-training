22f28050042ef65057121ff3e437934d3e916d6a
Rodion Minaev <r.minaev96@mail.ru>
2023-11-27 21:21:44 +0300

    Add features

--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -1,2 +1,23 @@
-package interfaces;public class Main {
+package interfaces;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Bird bird = new Bird();
+        
+//      В качетсве типа коллекции указываем родительский абстрактный класс Animal:
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(cat);
+        animals.add(dog);
+        animals.add(fish);
+        animals.add(bird);
+        
+        for(int i = 0; i < animals.size(); i++){
+            animals.get(i).eat();
+        }
+    }
 }
